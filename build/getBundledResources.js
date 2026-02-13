089669b9dc959f6fff9871cf4db649b0efb3567a
Mia Rosie Lleorenig <Mia.Lleorenig@nordicsemi.no>
2026-02-13 08:43:56 +0100

    fix: cleanup prefetched before pack
    This is necessary to avoid multiple JLink files in the build (mostly
    relevant for local builds)
    I simply remove the entire prefetched folder since we download all of it
    again anyway, but this could be improved in the future

--- build/getBundledResources.js
+++ build/getBundledResources.js
@@ -4,9 +4,16 @@
  * SPDX-License-Identifier: LicenseRef-Nordic-4-Clause
  */
 
+const { rm } = require('fs');
+const path = require('path');
 const { default: bundleApps } = require('./bundleApps');
 const { default: getJlink } = require('./getJlink');
 
 exports.default = async () => {
+    await rm(path.join('resources', 'prefetched'), {
+        force: true,
+        recursive: true,
+    });
+
     await Promise.allSettled([getJlink(), bundleApps()]);
 };
